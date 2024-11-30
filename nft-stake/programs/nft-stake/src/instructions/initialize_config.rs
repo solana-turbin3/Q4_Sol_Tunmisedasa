42791e3755d321ed6d03284be605d91355d72f80
tunmisedasa <tunmisan2k7@gmail.com>
2024-11-30 09:35:48 +0100

    add

--- nft-stake/programs/nft-stake/src/instructions/initialize_config.rs
+++ nft-stake/programs/nft-stake/src/instructions/initialize_config.rs
@@ -8,39 +8,35 @@ pub struct InitializeConfig<'info> {
     #[account(mut)]
     pub admin: Signer<'info>,
     #[account(
-        init,
+        init, 
         payer = admin,
         seeds = [b"config".as_ref()],
         bump,
-        space = StakeConfig::INIT_SPACE,
+        space = 8 + StakeConfig::INIT_SPACE,
     )]
     pub config: Account<'info, StakeConfig>,
-
     #[account(
         init,
         payer = admin,
         seeds = [b"rewards".as_ref(), config.key().as_ref()],
         bump,
         mint::decimals = 6,
-        mint::authority = config,
+        mint::authority = config
     )]
-    pub rewards_mint: Account<'info, Mint>,
+    pub rewards_mint: Account<'info, Mint>, 
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
 }
 
-
 impl<'info> InitializeConfig<'info> {
-    pub fn init_config(&mut self, points_per_stake: u8, max_stake: u8, freeze_period:u32, bumps: &InitializeConfigBumps) -> Result<()> {
-
-        self.config.set_inner(StakeConfig{
+    pub fn initialize_config(&mut self, points_per_stake: u8, max_stake: u8, freeze_period: u32, bumps: &InitializeConfigBumps) -> Result<()> {
+        self.config.set_inner(StakeConfig {
             points_per_stake,
             max_stake,
             freeze_period,
             rewards_bump: bumps.rewards_mint,
             bump: bumps.config,
         });
-
 
         Ok(())
     }
--- nft-stake/programs/nft-stake/src/lib.rs
+++ nft-stake/programs/nft-stake/src/lib.rs
@@ -4,7 +4,6 @@ pub mod instructions;
 pub mod state;
 
 
-
 use anchor_lang::prelude::*;
 
 
@@ -16,6 +15,27 @@ declare_id!("DGZg2jcbBMvPXvmcEiL5aF96JjDy6wmD39KNmR1u6aJZ");
 
 #[program]
 pub mod nft_stake {
+    use super::*;
+
+    pub fn initialize_config(ctx: Context<InitializeConfig>, points_per_stake: u8, max_stake: u8, freeze_period: u32) -> Result<()> {
+        ctx.accounts.initialize_config(points_per_stake, max_stake, freeze_period, &ctx.bumps)
+    }
+
+    pub fn initialize_user(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.initialize_user(&ctx.bumps)
+    }
+
+    pub fn stake(ctx: Context<Stake>) -> Result<()> {
+        ctx.accounts.stake(&ctx.bumps)
+    }
+
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
+        ctx.accounts.unstake()
+    }
+
+    pub fn claim(ctx: Context<Claim>) -> Result<()> {
+        ctx.accounts.claim()
+    }
 }
 
 #[derive(Accounts)]
